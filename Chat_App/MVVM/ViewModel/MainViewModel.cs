db9eb9fdc9eba01c3785d69c9c9e78407f082e02
hickee032 <hickee32@gmail.com>
2023-12-13 15:00:00 +0900

    Chat_App

--- Chat_App/MVVM/ViewModel/MainViewModel.cs
+++ Chat_App/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Chat_App.MVVM.Core;
+using Chat_App.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,12 @@ namespace Chat_App.MVVM.ViewModel
 {
     class MainViewModel
     {
-        public MainViewModel() {
+        public RelayCommand ConnectToServerCommand { get; set; }
 
+        private Server _server;
+        public MainViewModel() {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
         }
     }
 }
