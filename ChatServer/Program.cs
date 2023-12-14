e72a0e36fe17f87708930160fcc6344cefad484a
hickee032 <hickee32@gmail.com>
2023-12-14 15:00:00 +0900

    Chat_App

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatrSever
+{
+    class Program {
+
+        static TcpListener _listener;
+        static void Main(string[] args) {
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"),7891);
+            _listener.Start();
+
+            var client = _listener.AcceptTcpClient();
+            Console.WriteLine("Client has connected!");
+        }
+    }
+
+}
