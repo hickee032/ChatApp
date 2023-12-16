27aeebf41ebc620e984bb8a514da0d3bb1ddefa9
hickee032 <hickee32@gmail.com>
2023-12-16 15:00:00 +0900

    Chat_App

--- ChatServer/Client.cs
+++ ChatServer/Client.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatServer
 {
     class Client
     {
-        public Client() {
+        public string Username { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient ClientSocket { get; set; }
 
+        public Client(TcpClient client) {
+            ClientSocket = client;
+            UID = Guid.NewGuid();
+
+            Console.WriteLine($"[{DateTime.Now}]:Client has connected with the username: {Username}");
+            
         }
     }
 }
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatServer;
+using System;
 using System.Net;
 using System.Net.Sockets;
 
@@ -6,13 +7,16 @@ namespace ChatrSever
 {
     class Program {
 
+        static List<Client> _users;
         static TcpListener _listener;
         static void Main(string[] args) {
+
+            _users= new List<Client>();
             _listener = new TcpListener(IPAddress.Parse("127.0.0.1"),7891);
             _listener.Start();
 
-            var client = _listener.AcceptTcpClient();
-            Console.WriteLine("Client has connected!");
+            var client = new Client(_listener.AcceptTcpClient());
+            
         }
     }
 
