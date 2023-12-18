1c72a890f560aa3d60a78fda523aced5b6ed1b95
hickee032 <hickee32@gmail.com>
2023-12-18 15:00:00 +0900

    Chat_App

--- Chat_App/Net/IO/PacketBuilder.cs
+++ Chat_App/Net/IO/PacketBuilder.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chat_App.Net.IO
 {
-    internal class PacketBuilder
+    class PacketBuilder
     {
+        MemoryStream _ms;
+        public PacketBuilder() {
+            _ms= new MemoryStream();
+        }
+
+        public void WriteOpCode(byte opcode) {
+            _ms.WriteByte(opcode);
+        }
     }
 }
